62f29468aff7b7259bce36422d3a2dfaa63c4803
njurn <njurn@naver.com>
2013-11-05 20:43:16 +0900

    commit

---
+++ application/controllers/create.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Create extends CI_Controller {
+    function __construct() {
+        parent::__construct();
+		$this->load->helper('form');
+		$this->form_validation->set_error_delimiters('<dd class="error">','</dd>');		
+    }
+	
+	function index() {
+		/*if ($this->session->userdata('is_login')) {
+			redirect('/');    		
+    	}*/
+		
+		$this->load->view('create_v');
+	}
+	
+	//XXX: rename
+	function room() {
+		/*if ($this->session->userdata('is_login')) {
+			redirect('/');
+    	}*/
+		
+		echo $this->input->post('test');
+		
+		//echo $this->input->post('seats');
+		$seats = $this->input->post('seats');
+		foreach ($seats as $seat) {
+			echo $seat;
+		}
+	}
+}
+
+?>
