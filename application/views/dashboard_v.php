4d8b7fe0ca3ae113cccbea849b9e7d830f85635e
njurn <njurn@naver.com>
2013-11-06 15:37:13 +0900

    create group test

--- application/views/dashboard_v.php
+++ application/views/dashboard_v.php
@@ -22,7 +22,9 @@
 	<div id="groups-own">
 		<h2>Groups that <?=$this->session->userdata('name')?> owns</h2>
 		<div class="groups-box">
-			
+			<!-- TEST -->
+			<!--<a class="button" href="<?=site_url(array('group', 'create', $this->session->userdata('num')))?>">Create a new group</a>-->
+			<a class="button" href="<?=site_url(array('create', 'group', $this->session->userdata('num')))?>">Create a new group</a>
 		</div>
 	</div>
 </div>
